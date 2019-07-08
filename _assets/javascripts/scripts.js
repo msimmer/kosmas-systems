b20daa785a8e48c115ee261dfbacd5a0044297e1
Maxwell Simmer <maxwell.simmer@gmail.com>
2019-07-08 19:18:30 +0200

    Templates; nav

--- _assets/javascripts/scripts.js
+++ _assets/javascripts/scripts.js
@@ -1,10 +1,11 @@
-
 var MOBILE_SCREEN_WIDTH = 600
 
 $(function() {
     $('.nav__toggle').on('click', function() {
         $(this).toggleClass('open')
-        $(this).closest('.nav__main').toggleClass('open')
+        $(this)
+            .closest('.nav__main')
+            .toggleClass('open')
     })
 
     if (window.ksms && window.ksms.slider) {
@@ -21,30 +22,18 @@ $(function() {
         images.eq(index).show()
     }
 
-
-    // nav scroll
-    var sidebar = $('.nav__main__container')
-    var fixedSidebarClassName = 'nav__main__container--fixed'
-    function isMobile() {
-        return window.innerWidth <= MOBILE_SCREEN_WIDTH
-    }
-    function updateSidebarPosition() {
-        var headerHeight = $('.site__header').height()
-        if ($(window).scrollTop() > headerHeight) {
-            sidebar.addClass(fixedSidebarClassName)
+    // menu toggle
+    $('[data-nav-link]').on('click', function(e) {
+        e.preventDefault()
+        var name = $(this).attr('data-nav-link')
+        var list = $('[data-nav-list="' + name + '"]')
+        if (list.hasClass('open')) {
+            list.removeClass('open')
             return
         }
-
-        sidebar.removeClass(fixedSidebarClassName)
-
-    }
-    function resetSidebarPosition() {
-        sidebar.removeClass(fixedSidebarClassName)
-    }
-
-    $(window).on('scroll', function() {
-        if (isMobile()) return resetSidebarPosition()
-        updateSidebarPosition()
+        $('[data-nav-list]')
+            .not(list)
+            .removeClass('open')
+        list.addClass('open')
     })
-
 })
